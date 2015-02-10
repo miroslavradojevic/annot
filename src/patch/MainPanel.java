cd679fa87dff42e3ad12598f8ee0c20a87e3aaaf
GADEA MATA <gadea.mata@gmail.com>
2015-02-10 15:21:48 +0000

    MainPanel.java edited online with Bitbucket

--- src/patch/MainPanel.java
+++ src/patch/MainPanel.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ij.workspace.ANNOT;
+package patch;
 
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
