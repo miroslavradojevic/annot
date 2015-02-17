3e317b1a0ea5b5412222855335ec114a2a642bd3
Gadea Mata <Gadea Mata@localhost>
2015-02-17 14:13:01 +0100

    finished!

---
+++ src/patch/Annotationer.java
@@ -0,0 +1,382 @@
+package annot.src.patch;
+
+import ij.IJ;
+import ij.ImageListener;
+import ij.ImagePlus;
+import ij.Prefs;
+import ij.WindowManager;
+import ij.gui.*;
+import ij.io.FileSaver;
+import ij.io.OpenDialog;
+import ij.plugin.Duplicator;
+import ij.plugin.PlugIn;
+import ij.process.ByteProcessor;
+import ij.process.ImageProcessor;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Created by miroslav on 9-2-15.
+ */
+public class Annotationer implements PlugIn, MouseListener, MouseMotionListener {
+
+    ImagePlus inimg;
+    String[] namesGroups = {"Neuron", "Astrocyte", "Stop", "Cancel"};
+    int i, j, b = 0;
+//    Setting settings;
+    String image_path;
+    String path;
+    ImageCanvas canvas;
+//    ImageWindow wind;
+//    String[] namesGroups;
+    Overlay floating_ovl = new Overlay();
+
+//    float x1, y1, x2, y2;
+    //there are 7 different colors
+//    private static Color[] ColorArray = {new Color(1, 1, 0, 0.25f), new Color(1, 0, 1, 0.25f), new Color(1, 1, 1, 0.25f), new Color(1, 0, 0, 0.25f), new Color(0, 1, 0, 0.25f), new Color(0, 0, 1, 0.25f), new Color(0.96f, 0, 0.52f, 0.25f)};
+    int D, N;
+    int w, h;
+
+    ArrayList<Rectangle> NeuronList = new ArrayList<Rectangle>();
+    ArrayList<Rectangle> AstrocyteList = new ArrayList<Rectangle>();
+    ArrayList<Rectangle> BackList = new ArrayList<Rectangle>();
+
+    byte[] mask_obj; // will contain neurons and astrocytes tags
+    byte[] mask_temp; // will be assigned each time a new (random) was created
+
+    public void mouseClicked(MouseEvent e) {
+
+//        System.out.println("clicked");
+        float x = canvas.offScreenX(e.getX());
+        float y = canvas.offScreenY(e.getY());
+
+        GenericDialog gd = new GenericDialog("CHOOSE...");
+        gd.addChoice("choose ", namesGroups, namesGroups[0]);
+        gd.showDialog();
+        if (gd.wasCanceled()) {
+            
+            
+            return;
+        }
+        String aa = gd.getNextChoice();
+        int xRect = (int) (x - D / 2);
+        int yRect = (int) (y - D / 2);
+        if ((xRect < 0) || (yRect < 0)) {
+            return;
+        }
+
+        if (aa.equals(namesGroups[0])) {
+
+            Rectangle rect1 = new Rectangle(xRect, yRect, (int) D, (int) D);
+            NeuronList.add(rect1);
+
+            // update the map
+            for (int xloop = xRect; xloop < xRect + D; xloop++) {
+                for (int yloop = yRect; yloop < yRect + D; yloop++) {
+                    mask_obj[yloop * w + xloop] = (byte) 255;
+                }
+            }
+
+            // inteface thngy
+            Roi roi = new Roi(rect1);
+            roi.setStrokeColor(Color.red);
+            Roi last = floating_ovl.get(floating_ovl.size() - 1);
+            floating_ovl.remove(floating_ovl.size() - 1);
+            floating_ovl.add(roi);
+            floating_ovl.add(last);
+
+            inimg.setOverlay(floating_ovl);
+            inimg.updateAndDraw();
+
+            //to save the images
+            ImageProcessor ip = canvas.getImage().getChannelProcessor();
+            ImageProcessor ipCopy = ip.duplicate();
+            ipCopy.setRoi(roi);
+            ipCopy = ipCopy.crop();
+            ImagePlus impCopy = new ImagePlus(aa, ipCopy);
+            String auxPath = path + File.separator + aa;
+            File url = new File(auxPath);
+            url.mkdirs();
+            new FileSaver(impCopy).saveAsTiff(auxPath + File.separator + aa + "_" + i + ".tif");
+            i++;
+
+        } else if (aa.equals(namesGroups[1])) {
+            Rectangle rect1 = new Rectangle(xRect, yRect, (int) D, (int) D);
+            AstrocyteList.add(rect1);
+            // update the map
+            for (int xloop = xRect; xloop < xRect + D; xloop++) {
+                for (int yloop = yRect; yloop < yRect + D; yloop++) {
+                    mask_obj[yloop * w + xloop] = (byte) 255;
+                }
+            }
+
+            Roi roi = new Roi(rect1);
+            roi.setStrokeColor(Color.MAGENTA);
+            Roi last = floating_ovl.get(floating_ovl.size() - 1);
+            floating_ovl.remove(floating_ovl.size() - 1);
+            floating_ovl.add(roi);
+            floating_ovl.add(last);
+
+            inimg.setOverlay(floating_ovl);
+            inimg.updateAndDraw();
+            //to save the images
+            ImageProcessor ip = canvas.getImage().getChannelProcessor();
+            ImageProcessor ipCopy = ip.duplicate();
+            ipCopy.setRoi(roi);
+            ipCopy = ipCopy.crop();
+            ImagePlus impCopy = new ImagePlus(aa, ipCopy);
+            String auxPath = path + File.separator + aa;
+            File url = new File(auxPath);
+            url.mkdirs();
+            new FileSaver(impCopy).saveAsTiff(auxPath + File.separator + aa + "_" + j + ".tif");
+            j++;
+        } 
+        else if(aa.equals(namesGroups[2]))
+        {
+            randomBackground();
+            System.out.println(NeuronList.size() + "neurons");
+            System.out.println(AstrocyteList.size() + "astros");
+            System.out.println(BackList.size() + " backs");
+        }
+    }
+
+    public void mousePressed(MouseEvent e) {
+    }
+
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void mouseExited(MouseEvent e) {
+    }
+
+    public void mouseDragged(MouseEvent e) {
+    }
+
+    public void mouseMoved(MouseEvent e) {
+        float x = canvas.offScreenX(e.getX());
+        float y = canvas.offScreenY(e.getY());
+        PolygonRoi pr = new PolygonRoi(new float[]{x - .5f * D, x - .5f * D, x + .5f * D, x + .5f * D}, new float[]{y - .5f * D, y + .5f * D, y + .5f * D, y - .5f * D}, 4, PolygonRoi.POLYGON);
+        pr.setFillColor(new Color(1, 1, 0, 0.1f));
+        if (floating_ovl.size() > 0) {
+            floating_ovl.remove(floating_ovl.size() - 1);
+        }
+        floating_ovl.add(pr);
+//        System.out.println(floating_ovl.size());
+        canvas.setOverlay(floating_ovl);
+        inimg.updateAndDraw();
+    }
+
+    public void run(String s) {
+
+        NeuronList.clear();
+        AstrocyteList.clear();
+        BackList.clear();
+
+        //to open the dialog with the settings
+        MainPanel panelMain = new MainPanel();
+        GenericDialog gdG = new GenericDialog("Annotationer");
+        gdG.add(panelMain);
+        gdG.addMessage("");
+        MainPanel panelD = (MainPanel) gdG.getComponent(0);
+        gdG.showDialog();
+        if (gdG.wasCanceled()) {
+            return;
+        }
+//        settings = new Setting(panelD.getTxtUrl(), panelD.getTxtWidth());
+        path = panelD.getTxtUrl();
+        D = panelD.getTxtWidth();
+        N = panelD.getTxtNBackG();
+
+        if ((path.isEmpty())) {
+            IJ.error("You have to write an url for the results and the number of groups that you need to work.");
+            return;
+        }
+//        namesGroups = new String[settings.getNGroups()];
+//        for (int i = 0; i < settings.getNGroups(); i++) {
+//            namesGroups[i] = "Group " + (i + 1);
+//        }
+
+//        System.out.println("patch annotationer...");
+
+        // select the image
+        String in_folder = Prefs.get("id.folder", System.getProperty("user.home"));
+        OpenDialog.setDefaultDirectory(in_folder);
+        OpenDialog dc = new OpenDialog("Select file");
+        in_folder = dc.getDirectory();
+        image_path = dc.getPath();
+        if (image_path == null) {
+            return;
+        }
+        Prefs.set("id.folder", in_folder);
+
+        inimg = new ImagePlus(image_path);
+
+        if (inimg == null) {
+            return;
+        }
+        if (!getFileExtension(inimg.getTitle()).equalsIgnoreCase("TIF")) {
+            IJ.log("open image with .TIF extension");
+            return;
+        }
+
+//        IJ.log(image_path);
+        inimg.show();
+//        wind = inimg.getWindow();
+        canvas = inimg.getCanvas();
+//        ImageCanvas can = wind.getCanvas();
+
+//        wind.removeKeyListener(IJ.getInstance());
+//        can.removeKeyListener(IJ.getInstance());
+//        canvas.removeKeyListener(IJ.getInstance());
+        canvas.addMouseListener(this);
+        canvas.addMouseMotionListener(this);
+//        canvas.addKeyListener(this);
+//        wind.addKeyListener(this);
+//        ImagePlus.addImageListener(this);
+
+        w = inimg.getWidth();
+        h = inimg.getHeight();
+
+        mask_obj = new byte[w * h];
+        mask_temp = new byte[w * h];
+
+    }
+
+    public static String getFileExtension(String file_path) {
+        String extension = "";
+
+        int i = file_path.lastIndexOf('.');
+        if (i >= 0) {
+            extension = file_path.substring(i + 1);
+        }
+
+        return extension;
+    }
+
+    //the coordenates of both rois
+    private double overlap(Rectangle rect1, Rectangle rect2) {
+
+        double x11 = rect1.getX();//left -up
+        double y11 = rect1.getY();//left -up
+
+        double x12 = rect1.getX() + rect1.getWidth();//right -up
+
+        double y12 = rect1.getY() + rect1.getHeight();//left-down
+
+        double x21 = rect2.getX();//left -up
+        double y21 = rect2.getY();//left -up
+
+        double x22 = rect2.getX() + rect2.getWidth();//right -up
+
+        double y22 = rect2.getY() + rect2.getHeight();//left-down
+
+        double xDiff;
+        if (x11 < x21) {
+            xDiff = x12 - x21;
+        } else {
+            xDiff = x22 - x11;
+        }
+
+        double yDiff;
+        if (y11 < y21) {
+            yDiff = y12 - y21;
+        } else {
+            yDiff = y22 - y11;
+        }
+        xDiff = (xDiff < 0) ? 0 : xDiff;
+        yDiff = (yDiff < 0) ? 0 : yDiff;
+
+        return xDiff * yDiff;
+    }
+
+    private void randomBackground() {
+        Random r = new Random();
+        int count = 0;
+        int xR;
+        int yR;
+
+        byte[] uptillnow = new byte[mask_temp.length];
+
+        while (count < N) {
+
+            xR = r.nextInt(w);
+            yR = r.nextInt(h);
+
+            if ((xR + D < w) && (yR + D < h)) {
+                // fill the mask up
+                Arrays.fill(mask_temp, (byte) 0);
+                int cnt = 0;
+                int cnt_wrt_objects = 0;
+                for (int xloop = xR; xloop < xR + D; xloop++) {
+                    for (int yloop = yR; yloop < yR + D; yloop++) {
+                        mask_temp[yloop * w + xloop] = (byte) 255;
+                        if ((uptillnow[yloop * w + xloop] & 0xff) > 0 ) {
+                            cnt++;
+                        }
+                        if ( (mask_obj[yloop * w + xloop] & 0xff) > 0) {
+                            cnt_wrt_objects++;
+                        }
+                        
+                    }
+                }
+
+                if ((cnt / (float) (D * D)) < 0.1 && cnt_wrt_objects==0) {
+
+                    //                new ImagePlus("before", new ByteProcessor(w,h,mask_temp)).show();
+                    logic_or(uptillnow, mask_temp);
+
+//                new ImagePlus("after", new ByteProcessor(w,h,mask_temp)).show();
+//                    for (int k = 0; k < uptillnow.length; k++) {
+//                        uptillnow[k] = mask_temp[k];
+//                    }
+
+                    Rectangle rectRand = new Rectangle(xR, yR, D, D);
+                    BackList.add(rectRand);
+                    Roi roiRand = new Roi(rectRand);
+                    inimg.setRoi(roiRand);
+//                    System.out.println("" + count);
+                    roiRand.setStrokeColor(Color.blue);
+                    Roi last = floating_ovl.get(floating_ovl.size() - 1);
+                    floating_ovl.remove(floating_ovl.size() - 1);
+                    floating_ovl.add(roiRand);
+                    floating_ovl.add(last);
+
+                    inimg.setOverlay(floating_ovl);
+                    inimg.updateAndDraw();
+
+                    //to save the images
+                    ImageProcessor ip = canvas.getImage().getChannelProcessor();
+                    ImageProcessor ipCopy = ip.duplicate();
+                    ipCopy.setRoi(roiRand);
+                    ipCopy = ipCopy.crop();
+                    ImagePlus impCopy = new ImagePlus("Background", ipCopy);
+                    String auxPath = path + File.separator + "Background";
+                    File url = new File(auxPath);
+                    url.mkdirs();
+                    new FileSaver(impCopy).saveAsTiff(auxPath + File.separator + "Background" + "_" + b + ".tif");
+                    b++;
+
+                    count++;
+
+                }
+
+            }
+        }
+
+//        new ImagePlus("aaa", new ByteProcessor(w, h, uptillnow)).show();
+    }
+
+    private void logic_or(byte[] a, byte[] b) { // final output is stored in a
+        for (int k = 0; k < a.length; k++) {
+            a[k] = ((a[k] & 0xff) > (b[k] & 0xff)) ? a[k] : b[k];
+        }
+    }
+}
